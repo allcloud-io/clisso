b396fe410f90c294df276f7785281649192a5778
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-04-03 21:42:11 +0300

    Extract access_token from response

--- generate_token.sh
+++ generate_token.sh
@@ -8,10 +8,12 @@ fi
 client_secret=$1
 client_id=$2
 
-curl 'https://api.us.onelogin.com/auth/oauth2/token' \
+response=$(curl -s 'https://api.us.onelogin.com/auth/oauth2/token' \
 -X POST \
 -H "Authorization: client_id:$client_id, client_secret:$client_secret" \
 -H "Content-Type: application/json" \
 -d '{ 
     "grant_type":"client_credentials"
-}'
+}')
+
+echo $response | jq '.data[] .access_token' | tr -d '"'
