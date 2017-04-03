87028c640bd7e355b1e23f04cc237d3da0d6ee5b
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-04-03 21:35:42 +0300

    Initial commit

---
+++ generate_token.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <client_secret> <client_id>"
+    exit 1
+fi
+
+client_secret=$1
+client_id=$2
+
+curl 'https://api.us.onelogin.com/auth/oauth2/token' \
+-X POST \
+-H "Authorization: client_id:$client_id, client_secret:$client_secret" \
+-H "Content-Type: application/json" \
+-d '{ 
+    "grant_type":"client_credentials"
+}'
