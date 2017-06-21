bf2402d4d08ed707ad54ddf17764fa7f2a1e7670
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-06-22 00:14:01 +0300

    Improve help text for csso get

--- main.go
+++ main.go
@@ -147,9 +147,9 @@ func main() {
 	var cmdGet = &cobra.Command{
 		Use:   "get",
 		Short: "Get temporary credentials",
-		Long: `Obtain temporary credentials for the currently-selected account
-		by generating a SAML assertion at the identity provider and using this
-		assertion to retrieve temporary credentials from the cloud vendor.`,
+		Long: `Obtain temporary credentials for the currently-selected account by
+generating a SAML assertion at the identity provider and using this
+assertion to retrieve temporary credentials from the cloud provider.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			get()
 		},
