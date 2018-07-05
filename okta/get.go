d6188d5b0d1c77cf22c310872ed756245c74530e
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-07-05 20:09:06 +0200

    Add TODO

--- okta/get.go
+++ okta/get.go
@@ -4,6 +4,7 @@ import awsprovider "github.com/allcloud-io/clisso/aws"
 
 // Get gets temporary credentials for the given app.
 func Get(app, provider string) (*awsprovider.Credentials, error) {
+	// TODO Implement Okta logic
 	return &awsprovider.Credentials{
 		AccessKeyID: "fake", SecretAccessKey: "fake", SessionToken: "fake",
 	}, nil
