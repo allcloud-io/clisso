132df28b4a3224bd93715784aa31ff452e034534
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-07-08 12:36:54 +0200

    Add skeleton for okta.Get

--- okta/get.go
+++ okta/get.go
@@ -4,7 +4,26 @@ import awsprovider "github.com/allcloud-io/clisso/aws"
 
 // Get gets temporary credentials for the given app.
 func Get(app, provider string) (*awsprovider.Credentials, error) {
-	// TODO Implement Okta logic
+	// Get provider config
+
+	// Get app config
+
+	// Initialize Okta client
+
+	// Get session token
+
+	// Verify MFA
+
+	// Launch Okta app with session token
+
+	// Store cookie
+
+	// Follow redirect link(s) to SAML endpoint
+
+	// Extract SAML from response (HTML)
+
+	// Assume role
+
 	return &awsprovider.Credentials{
 		AccessKeyID: "fake", SecretAccessKey: "fake", SessionToken: "fake",
 	}, nil
