1d77a559f17eb282267919a1fd85b3d2ef3dc56f
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-07-08 15:28:28 +0200

    Implement GetSessionToken

---
+++ okta/client.go
@@ -0,0 +1,99 @@
+package okta
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+// Client represents an Okta API client.
+type Client struct {
+	http.Client
+	BaseURL string
+}
+
+// GetSessionTokenParams represents the parameters for GetSessionToken.
+type GetSessionTokenParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// GetSessionTokenResponse represents the result of GetSessionToken.
+type GetSessionTokenResponse struct {
+	ExpiresAt    time.Time
+	Status       string
+	SessionToken string
+}
+
+// GetSessionToken performs a login operation against the Okta API and returns a session token upon
+// successful login.
+func (c *Client) GetSessionToken(p *GetSessionTokenParams) (string, error) {
+	h := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	req, err := makeRequest(http.MethodPost, c.BaseURL+"/api/v1/authn", h, p)
+	if err != nil {
+		return "", fmt.Errorf("creating request: %v", err)
+	}
+
+	data, err := c.doRequest(req)
+	if err != nil {
+		return "", fmt.Errorf("doing HTTP request: %v", err)
+	}
+
+	var resp GetSessionTokenResponse
+	if err := json.Unmarshal([]byte(data), &resp); err != nil {
+		return "", fmt.Errorf("parsing HTTP response: %v", err)
+	}
+
+	return resp.SessionToken, nil
+}
+
+// doRequest gets a pointer to an HTTP request and an HTTP client, executes the request
+// using the client, handles any HTTP-related errors and returns any data as a string.
+func (c *Client) doRequest(r *http.Request) (string, error) {
+	resp, err := c.Do(r)
+	if err != nil {
+		return "", fmt.Errorf("sending HTTP request: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		return "", errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	b := []byte(body)
+
+	return string(b), nil
+}
+
+// NewClient creates a new Client and returns a pointer to it.
+func NewClient(url string) *Client {
+	return &Client{BaseURL: url}
+}
+
+// makeRequest constructs an HTTP request and returns a pointer to it.
+// TODO Wrap arguments in a type
+func makeRequest(method string, url string, headers map[string]string, body interface{}) (*http.Request, error) {
+	json, err := json.Marshal(body)
+	if err != nil {
+		return nil, fmt.Errorf("parsing body: %v", err)
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(json))
+	if err != nil {
+		return nil, fmt.Errorf("making HTTP request: %v", err)
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	return req, nil
+}
--- okta/get.go
+++ okta/get.go
@@ -2,9 +2,11 @@ package okta
 
 import (
 	"fmt"
+	"log"
 
 	awsprovider "github.com/allcloud-io/clisso/aws"
 	"github.com/allcloud-io/clisso/config"
+	"github.com/howeyc/gopass"
 )
 
 // Get gets temporary credentials for the given app.
@@ -16,14 +18,38 @@ func Get(app, provider string) (*awsprovider.Credentials, error) {
 	}
 
 	// Get app config
-	a, err := config.GetOktaApp(app)
-	if err != nil {
-		return nil, fmt.Errorf("reading config for app %s: %v", app, err)
-	}
+	// a, err := config.GetOktaApp(app)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("reading config for app %s: %v", app, err)
+	// }
 
 	// Initialize Okta client
+	c := NewClient(p.BaseURL)
+
+	// Get user credentials
+	user := p.Username
+	if user == "" {
+		// Get credentials from the user
+		fmt.Print("Okta username: ")
+		fmt.Scanln(&user)
+	}
+
+	fmt.Print("Okta password: ")
+	pass, err := gopass.GetPasswd()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't read password from terminal")
+	}
 
 	// Get session token
+	params := GetSessionTokenParams{
+		Username: user,
+		Password: string(pass),
+	}
+	t, err := c.GetSessionToken(&params)
+	if err != nil {
+		log.Fatalf("getting session token: %v", err)
+	}
+	log.Printf("Session token: %s", t)
 
 	// Verify MFA
 
