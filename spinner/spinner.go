1473540f0a2de1494890c5849125bd4d86a9a57d
Jonathan Vogt <jonathan.vogt@allcloud.io>
2018-09-07 14:57:04 +0200

    adding Plan9 support

--- spinner/spinner.go
+++ spinner/spinner.go
@@ -1,12 +1,13 @@
 package spinner
 
-// This is a wrapper around spinner to support different operation systems until upstream is fixed.
+// This is a wrapper around spinner to disable unsupported operation systems transparently until upstream is fixed.
+// See https://github.com/briandowns/spinner/issues/52
 
 func New() SpinnerWrapper {
 	return new()
 }
 
-// SpinnerWrapper is used to abstract a spinner so that it can be conveniently disabled on Windows.
+// SpinnerWrapper is used to abstract a spinner so that it can be conveniently disabled on terminals which don't support it.
 type SpinnerWrapper interface {
 	Start()
 	Stop()
--- spinner/spinner_windows.go
+++ spinner/spinner_windows.go
@@ -8,6 +8,7 @@ func new() SpinnerWrapper {
 
 // noopSpinner is a mock spinner which doesn't do anything. It is used to centrally disable the
 // spinner on Windows (because it isn't supported by the Windows terminal).
+// See https://github.com/briandowns/spinner/issues/52
 type noopSpinner struct{}
 
 func (s *noopSpinner) Start() {}
