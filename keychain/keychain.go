9572b15edea688885ffc8628b9ac5fa84b752916
Jonathan Vogt <jonathan.vogt@allcloud.io>
2018-09-12 16:43:52 +0200

    fix windows build is broken

--- keychain/keychain.go
+++ keychain/keychain.go
@@ -1,14 +1,5 @@
 package keychain
 
-import (
-	"errors"
-	"log"
-	"runtime"
-
-	"github.com/fatih/color"
-	"github.com/tmc/keyring"
-)
-
 const (
 	// KeyChainName is the name of the keychain used to store
 	// passwords
@@ -29,20 +20,11 @@ type DefaultKeychain struct{}
 // Set takes a provider in an argument, and a password from STDIN, and
 // sets it in a keychain, should one exist.
 func (DefaultKeychain) Set(provider string, password []byte) (err error) {
-	if runtime.GOOS == "windows" {
-		log.Fatal(color.RedString("Storing passwords is not supported on windows"))
-	}
-	return keyring.Set(KeyChainName, provider, string(password))
+	return set(provider, password)
 }
 
 // Get will, once given a valid provider, return the password associated
 // in order for logins to happen
 func (DefaultKeychain) Get(provider string) (pw []byte, err error) {
-	if runtime.GOOS == "windows" {
-		return nil, errors.New("Platform is not supported yet")
-	}
-	pwString, err := keyring.Get(KeyChainName, provider)
-	pw = []byte(pwString)
-
-	return
+	return get(provider)
 }
