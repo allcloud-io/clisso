ea0f7b3862a1d6c72f678cb0d7e117cedda21ead
Jonathan Vogt <jonathan.vogt@allcloud.io>
2018-10-29 19:28:02 +0100

    fix long lines

--- okta/get.go
+++ okta/get.go
@@ -107,8 +107,10 @@ func Get(app, provider string, duration int64) (*aws.Credentials, error) {
 	// the default duration might be shorter than what is configured on AWS side. The code above
 	// selected the minimum duration. If more was requested print an info.
 	if err == aws.ErrInvalidSessionDuration {
-		fmt.Printf("The role does not support the requested duration of %v. To have a max session duration for up to 12h run:\n", duration)
-		fmt.Printf("aws iam update-role --role-name %v --max-session-duration 43200 --profile %v\n", arn.Role[strings.LastIndex(arn.Role, "/")+1:], app)
+		fmt.Printf("The role does not support the requested duration of %v. To have a max session "+
+			"duration for up to 12h run:\n", duration)
+		fmt.Printf("aws iam update-role --role-name %v --max-session-duration 43200 --profile %v\n",
+			arn.Role[strings.LastIndex(arn.Role, "/")+1:], app)
 		err = nil
 	}
 
