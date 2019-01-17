6de00d72d9e361f3457ad0518a37f0dfaab6563e
Jonathan Vogt <jonathan.vogt@allcloud.io>
2019-01-17 16:26:03 +0100

    fix no roles returned even if they have been returned

--- saml/saml.go
+++ saml/saml.go
@@ -33,7 +33,7 @@ func Get(data string) (a ARN, err error) {
 
 	switch len(arns) {
 	case 0:
-		err = errors.New("no valid AWS roles were returned")
+		err = errors.New("no valid AWS roles were returned, check for extra characters on the IDP config")
 
 		return
 
@@ -68,7 +68,7 @@ func extractArns(attrs []saml.Attribute) (arns []ARN) {
 				// 1. arn:aws:iam::xxxxxxxxxxxx:role/MyRole,arn:aws:iam::xxxxxxxxxxxx:saml-provider/MyProvider
 				// 2. arn:aws:iam::xxxxxxxxxxxx:saml-provider/MyProvider,arn:aws:iam::xxxxxxxxxxxx:role/MyRole
 				// Error otherwise.
-				components := strings.Split(av.Value, ",")
+				components := strings.Split(strings.TrimSpace(av.Value), ",")
 				if len(components) != 2 {
 					// Wrong number of components - move on
 					continue
